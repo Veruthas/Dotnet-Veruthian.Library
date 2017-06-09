aa7e42a06a874de646772b3a06d28cac528d6495
Veruthas <Veruthas@gmail.com>
2017-06-09 14:52:18 -0400

    Added Buffer code and Token abstract methods to Lexer.

--- Solution/Projects/Soedeum.Dotnet.Library/Compilers/Lexers/Lexer.cs
+++ Solution/Projects/Soedeum.Dotnet.Library/Compilers/Lexers/Lexer.cs
@@ -7,7 +7,7 @@ using Soedeum.Dotnet.Library.Text;
 
 namespace Soedeum.Dotnet.Library.Compilers.Lexers
 {
-    public class Lexer<TToken, TTokenType, TReader> : Lexer<TToken, TTokenType, TReader, Object>
+    public abstract class Lexer<TToken, TTokenType, TReader> : Lexer<TToken, TTokenType, TReader, Object>
         where TToken : IToken<TTokenType>
         where TTokenType : IEquatable<TTokenType>
         where TReader : IReader<char>
@@ -18,7 +18,7 @@ namespace Soedeum.Dotnet.Library.Compilers.Lexers
     }
 
 
-    public class Lexer<TToken, TTokenType, TReader, TState> : IEnumerator<TToken>
+    public abstract class Lexer<TToken, TTokenType, TReader, TState> : IEnumerator<TToken>
         where TToken : IToken<TTokenType>
         where TTokenType : IEquatable<TTokenType>
         where TReader : IReader<char>
@@ -74,16 +74,39 @@ namespace Soedeum.Dotnet.Library.Compilers.Lexers
             throw new NotImplementedException();
         }
 
+        // Token code
+        protected abstract TToken CreateEofToken(TextLocation location);
+
+        protected abstract TToken GetNextToken();
+
+        protected abstract TToken CreateToken(TTokenType tokenType, TextLocation location, string value);
+
+        // Todo add default string check?
+        protected virtual TToken CreateTokenFromBuffer(TTokenType tokenType)
+        {
+            return CreateToken(tokenType, bufferLocation, Extract());
+        }
 
         // Buffer code
         protected void Capture()
         {
-
+            Release();
+            capturing = true;
         }
 
         protected void Release()
         {
+            buffer.Clear();
+            bufferLocation = default(TextLocation);
+            capturing = false;
+        }
 
+        protected string Extract()
+        {
+            if (!capturing)
+                throw new InvalidOperationException("Buffer is not capturing!");
+
+            return buffer.ToString();
         }
 
         protected virtual void AttachBufferEvents()
@@ -103,8 +126,15 @@ namespace Soedeum.Dotnet.Library.Compilers.Lexers
                                             int originalPosition,
                                             TState originalState)
         {
-            int length = fromPosition - originalPosition;
+            if (capturing)
+            {
+                int length = fromPosition - originalPosition;
 
+                if (length > buffer.Length)
+                    Release();
+                else
+                    buffer.Remove(buffer.Length - length, length);
+            }
         }
 
         protected virtual void OnRead(IReader<char> reader, char item)
