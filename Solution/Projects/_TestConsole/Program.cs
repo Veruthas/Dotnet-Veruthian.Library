df96ce56ec9ea6bd34d7988c9c1b55b388edc1c3
Veruthas <Veruthas@gmail.com>
2017-07-31 02:49:07 -0400

    Removed old pattern namespace.

--- Solution/Projects/_TestConsole/Program.cs
+++ Solution/Projects/_TestConsole/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Text;
 using Soedeum.Dotnet.Library;
 using Soedeum.Dotnet.Library.Data;
-using Soedeum.Dotnet.Library.Data.Patterns;
 using Soedeum.Dotnet.Library.Data.Ranges;
 using Soedeum.Dotnet.Library.Numeric;
 using Soedeum.Dotnet.Library.Text.Code;
@@ -16,18 +15,6 @@ namespace _TestConsole
     {
         static void Main(string[] args)
         {
-            var a = new PatternBuilder<CodeSet>(CodeSet.IdentifierFirst);
-
-            var b = new PatternBuilder<CodeSet>(CodeSet.IdentifierFollow).MakeOptional().MakeRepeating();
-
-            var c = a + b;
-
-            Console.WriteLine(a);
-
-            Console.WriteLine(b);
-
-            Console.WriteLine(c);
-
             Pause();
         }
 
