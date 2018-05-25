04d52773019c5f5292b49a55398bf8539982ada7
Veruthas <veruthas@gmail.com>
2018-05-25 05:15:24 -0400

    Wrote failed end test.

--- Solution/Projects/Veruthian.Dotnet.Library.Tests/Data/Readers/ReadersTest.cs
+++ Solution/Projects/Veruthian.Dotnet.Library.Tests/Data/Readers/ReadersTest.cs
@@ -1,7 +1,28 @@
-namespace Veruthian.Dotnet.Library.Tests.Data.Readers
+using System;
+using System.Collections.Generic;
+using Veruthian.Dotnet.Library.Text.Code;
+using Xunit;
+
+namespace Veruthian.Dotnet.Library.Data.Readers
 {
     public class ReadersTest
     {
-        
+        [InlineData("")]
+        [InlineData("Hello")]
+        [Theory]
+        public void TestSimpleReader(string data)
+        {
+            var codes = data.ToCodePoints();
+
+            var reader = codes.GetSimpleReader();
+
+            foreach(var c in data)
+            {
+                Assert.Equal(c, reader.Read());                
+            }
+
+            Assert.Equal(reader.Read(), new char());
+            Assert.True(reader.IsEnd);
+        }
     }
 }
