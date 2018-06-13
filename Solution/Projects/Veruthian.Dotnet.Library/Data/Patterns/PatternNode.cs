b63658e372e9717154bda25d54abf608b00683d3
Veruthas <veruthas@gmail.com>
2018-06-13 19:11:32 -0400

    Added PatternNode interface.

---
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Patterns/PatternNode.cs
@@ -0,0 +1,14 @@
+namespace Veruthian.Dotnet.Library.Data.Patterns
+{
+    public interface IPatternNode<TNode, TType> : IEnumerable<TNode>
+        where TNode : IPatternNode<TNode, TType>
+    {
+        TType Type { get; }
+
+        bool HasChildren { get; }
+
+        int ChildCount { get; }
+
+        TNode GetChild(int index);
+    }
+}
