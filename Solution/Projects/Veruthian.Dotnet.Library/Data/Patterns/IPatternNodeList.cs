e932d12cae8f85bc67abbe4545e5c0d675ceaed6
Veruthas <veruthas@gmail.com>
2018-06-16 22:32:38 -0400

    Pre-Collections.

--- Solution/Projects/Veruthian.Dotnet.Library/Data/Patterns/IPatternNodeList.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Patterns/IPatternNodeList.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace Veruthian.Dotnet.Library.Data.Patterns
 {
-    public interface IPatternNodeList<TNode, TType, TData>
+    public interface IPatternNodeList<TNode, TType, TData> : IEnumerable<TNode>
         where TNode : IPatternNode<TNode, TType, TData>
     {
-         
+        int Count { get; }
+
+        TNode this[int index] { get; }
     }
 }
