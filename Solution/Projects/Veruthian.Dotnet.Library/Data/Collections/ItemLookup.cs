417566f7c85ced242f5acf3192b781a54fdef5a6
Veruthas <veruthas@gmail.com>
2018-06-19 03:12:59 -0400

    Added defaultable to all classes.

--- Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/ItemLookup.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/ItemLookup.cs
@@ -6,16 +6,26 @@ namespace Veruthian.Dotnet.Library.Data.Collections
     {
         Dictionary<TKey, TValue> items;
 
+        bool defaultable;
+
+
+        public ItemLookup() { }
+
+        public ItemLookup(bool defaultable) => this.defaultable = defaultable;
+
 
         public TValue this[TKey key]
         {
-            get => items[key];
-            set => items[key] = value;
+            get => TryGet(key, out var value) ? value : defaultable ? default(TValue) : throw new KeyNotFoundException($"{key?.ToString() ?? ""} is not define.");
+            set
+            {
+                items[key] = value;
+            }
         }
 
         TValue ILookup<TKey, TValue>.this[TKey key] => this[key];
 
-        public bool IsDefaultable => false;
+        public bool IsDefaultable => defaultable;
 
         public int Count => items.Count;
 
