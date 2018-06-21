28a0492b9777b1a4c3e9dc4cdb83f7c85bda2104
Veruthas <veruthas@gmail.com>
2018-06-21 02:59:54 -0400

    Cleaning up again :|, adding Index interfaces.

--- Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/IContainer.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/IContainer.cs
@@ -2,6 +2,20 @@ using System.Collections.Generic;
 
 namespace Veruthian.Dotnet.Library.Data.Collections
 {
+    /* 
+        IContainer<T>
+        IExpandableContainer<T>
+
+        ILookup<K, V> : IContainer<V>
+        IMutableLookup<K, V> : ILookup<K, V>
+        IExpandableLookup<K, V> : IMutableLookup<K, V>
+
+        IIndex<T> : ILookup<int, T>        
+        IMutableIndex<T> : IIndex<T>, IMutableLookup<int, T>
+        IOrderedIndex<T> : IIndex<T>, IExpandableContainer<T>
+        IExpandableIndex<T> : IMutableIndex<T>, IExpandableLookup<int, T>, IExpandableContainer<T>
+    */
+
     public interface IContainer<T>
     {
         int Count { get; }
---
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/IMutableIndex.cs
@@ -0,0 +1,7 @@
+namespace Veruthian.Dotnet.Library.Data.Collections
+{
+    public interface IMutableIndex<T> : IIndex<T>, IMutableLookup<int, T>
+    {
+
+    }
+}
