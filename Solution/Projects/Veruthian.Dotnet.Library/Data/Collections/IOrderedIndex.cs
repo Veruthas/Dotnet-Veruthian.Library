f4935511779a8e579dbb84739a712718d220eded
Veruthas <veruthas@gmail.com>
2018-06-21 19:21:25 -0400

    Implemented Ordereds.

--- Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/IOrderedIndex.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Data/Collections/IOrderedIndex.cs
@@ -2,6 +2,6 @@ namespace Veruthian.Dotnet.Library.Data.Collections
 {
     public interface IOrderedIndex<T> : IIndex<T>, IExpandableContainer<T>
     {
-
+        void RemoveBy(int index);
     }
 }
