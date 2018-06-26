91bd5a787fe2ed717314b510875b6a4c1b097816
Veruthas <Veruthas@gmail.com>
2018-06-26 19:21:16 -0400

    Removed struct requirement.

--- Solution/Projects/Veruthian.Dotnet.Library/Numeric/INumeric.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Numeric/INumeric.cs
@@ -1,18 +1,20 @@
 namespace Veruthian.Dotnet.Library.Numeric
 {
     public interface INumeric<N> : IOrderable<N>
-        where N : struct, INumeric<N>
+        where N : INumeric<N>
     {
         N Add(N value);
 
         N Subtract(N value);
 
+        N Delta(N value);
+
         N Multiply(N value);
 
-        N Divide(N value);
+        N Divide(N value);        
+
+        N Divide(N value, out N remainder);
 
         N Modulus(N value);
-
-        N DivideWithRemainder(N value, out N remainder);        
     }
 }
