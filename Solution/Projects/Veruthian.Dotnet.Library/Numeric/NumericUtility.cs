7d5d2c426d028e5fd0ed85cd129c88bcaa637e34
Veruthas <veruthas@gmail.com>
2018-06-28 04:09:04 -0400

    Moved add methods to NumericUtility.

--- Solution/Projects/Veruthian.Dotnet.Library/Numeric/NumericUtility.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Numeric/NumericUtility.cs
@@ -8,5 +8,30 @@ namespace Veruthian.Dotnet.Library.Numeric
 
             return ((uint)result, (uint)(result >> 32));
         }
+
+        public static uint[] Add(uint value, uint[] values)
+        {
+            int oldSize = values.Length;
+
+            bool overflow = (values[oldSize - 1] == uint.MaxValue);
+
+            var newValues = new uint[overflow ? oldSize : oldSize + 1];
+
+            uint carry = value;
+
+            for (int i = 0; i < oldSize; i++)
+                (newValues[i], carry) = NumericUtility.Add(values[i], carry);
+
+            if (overflow)
+                newValues[oldSize] = carry;
+
+            return newValues;
+        }
+
+        public static uint[] Add(uint[] left, uint[] right)
+        {
+            return new uint[0];
+        }
+
     }
 }
