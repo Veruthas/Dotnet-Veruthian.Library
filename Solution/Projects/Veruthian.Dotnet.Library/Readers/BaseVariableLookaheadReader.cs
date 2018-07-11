dfc9b3262f1a322e572ebd438fa2b68562f67652
Veruthas <Veruthas@gmail.com>
2018-07-11 12:54:23 -0400

    Missed two Base Suffix to Prefix changes.

--- Solution/Projects/Veruthian.Dotnet.Library/Readers/BaseVariableLookaheadReader.cs
+++ Solution/Projects/Veruthian.Dotnet.Library/Readers/BaseVariableLookaheadReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Veruthian.Dotnet.Library.Readers
 {
-    public abstract class BaseVariableLookaheadReader<T> : LookaheadReaderBase<T>
+    public abstract class BaseVariableLookaheadReader<T> : BaseLookaheadReader<T>
     {
         List<T> items = new List<T>();
 
