b9890a576940ec4b54d48eeaa79bf5f971f9c0c2
Veruthas <veruthas@gmail.com>
2019-03-14 02:52:17 -0400

    Fixed up stream extensions.

--- Solution/Projects/Veruthian.Library/Processing/Extensions/StreamExtensions.cs
+++ Solution/Projects/Veruthian.Library/Processing/Extensions/StreamExtensions.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+using System.IO;
+
 namespace Veruthian.Library.Processing.Extensions
 {
     public static class StreamExtensions
     {
-        
+        public static IEnumerable<byte> GetBytes(this Stream stream)
+        {
+            var item = stream.ReadByte();
+
+            while (item != -1)
+            {
+                yield return (byte)item;
+            }
+        }
+
+        public static IEnumerable<byte> GetBytes(string path)
+        {
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                return GetBytes(stream);
+            }
+        }
     }
 }
