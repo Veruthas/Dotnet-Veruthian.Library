8a06d9a1879230cd4a291c1caabd3ed189becc9a
Veruthas <veruthas@gmail.com>
2019-03-15 03:30:55 -0400

    Cleaned up program.cs

--- Solution/Projects/_Console/Program.cs
+++ Solution/Projects/_Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using Veruthian.Library.Text.Chars;
-using Veruthian.Library.Text.Chars.Extensions;
-using Veruthian.Library.Text.Runes;
-using Veruthian.Library.Text.Runes.Extensions;
 
 namespace _Console
 {
@@ -10,19 +6,6 @@ namespace _Console
     {
         static void Main(string[] args)
         {
-            RuneLineTable lines;
-
-            foreach (var r in "Hello\rworld\r\nLevi".ToRuneString().ProcessLines(out lines))
-            {
-                Console.WriteLine(r.ToPrintableString());
-            }
-            Console.WriteLine();
-
-            foreach (var rs in lines.ExtractLines("Hello\rworld\r\nLevi", true))
-            {
-                Console.WriteLine(rs.ToPrintableString());
-            }
-
             Pause();
         }
 
