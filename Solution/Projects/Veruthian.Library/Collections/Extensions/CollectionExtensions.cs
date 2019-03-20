f39859b015152863b952c322a04289ff79403019
Veruthas <veruthas@gmail.com>
2019-03-20 16:53:51 -0400

    Added extension methods, made DataList consistent

--- Solution/Projects/Veruthian.Library/Collections/Extensions/CollectionExtensions.cs
+++ Solution/Projects/Veruthian.Library/Collections/Extensions/CollectionExtensions.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Veruthian.Library.Collections.Extensions
 {
     public static class CollectionExtensions
@@ -6,7 +8,20 @@ namespace Veruthian.Library.Collections.Extensions
 
         public static DataArray<T> ToDataArray<T>(this IContainer<T> items) => DataArray<T>.Extract(items, items.Count);
 
-        public static DataList<T> ToDataList<T>(this IContainer<T> items) => new DataList<T>(items);
+        public static DataList<T> ToDataList<T>(this IContainer<T> items) => DataList<T>.Extract(items);
 
+
+        public static DataIndex<T> ToDataIndex<T>(this IEnumerable<T> items) => DataIndex<T>.Extract(items);
+
+        public static DataArray<T> ToDataArray<T>(this IEnumerable<T> items) => DataArray<T>.Extract(items);
+
+        public static DataList<T> ToDataList<T>(this IEnumerable<T> items) => DataList<T>.Extract(items);
+
+
+        public static DataIndex<T> ToDataIndex<T>(this T[] items) => DataIndex<T>.From(items);
+
+        public static DataArray<T> ToDataArray<T>(this T[] items) => DataArray<T>.From(items);
+
+        public static DataList<T> ToDataList<T>(this T[] items) => DataList<T>.From(items);
     }
 }
