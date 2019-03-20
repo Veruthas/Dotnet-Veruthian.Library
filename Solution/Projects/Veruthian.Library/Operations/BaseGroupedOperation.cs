d0953df5d83c8aa3b3de8590807dcb66aafe6b7c
Veruthas <veruthas@gmail.com>
2019-03-20 15:55:08 -0400

    Added some functionality to SequentialOperation

--- Solution/Projects/Veruthian.Library/Operations/BaseGroupedOperation.cs
+++ Solution/Projects/Veruthian.Library/Operations/BaseGroupedOperation.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
+using Veruthian.Library.Collections;
 
 namespace Veruthian.Library.Operations
 {
     public abstract class BaseGroupedOperation<TState> : BaseOperation<TState>
     {
-        private IOperation<TState>[] operations;
+        private readonly DataIndex<IOperation<TState>> operations;
 
 
-        protected override int Count => operations.Length;
+        public BaseGroupedOperation(DataIndex<IOperation<TState>> operations) => this.operations = operations;
+
+
+        protected DataIndex<IOperation<TState>> Operations => operations;
+
+        protected override int Count => operations.Count;
 
         protected override IOperation<TState> GetSubOperation(int verifiedIndex) => operations[verifiedIndex];
     }
--- Solution/Projects/Veruthian.Library/Operations/SequentialOperation.cs
+++ Solution/Projects/Veruthian.Library/Operations/SequentialOperation.cs
@@ -1,7 +1,26 @@
+using Veruthian.Library.Collections;
+using Veruthian.Library.Collections.Extensions;
+
 namespace Veruthian.Library.Operations
 {
-    public class SequentialOperation<TState>
+    public class SequentialOperation<TState> : BaseGroupedOperation<TState>
     {
-        
+        public SequentialOperation(DataIndex<IOperation<TState>> operations) : base(operations)
+        {
+        }
+
+
+        public override string Description => this.ToListString("(", ")", " ");
+
+        protected override bool DoAction(TState state, ITracer<TState> tracer = null)
+        {
+            foreach(var operation in Operations)
+            {
+                if (!operation.Perform(state, tracer))
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
