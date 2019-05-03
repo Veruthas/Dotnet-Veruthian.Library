348eddf2c160b38001bfcbb25e10211e07d84c42
Veruthas <veruthas@gmail.com>
2019-05-03 13:06:01 -0400

    Added OptionalOperation.

---
+++ Solution/Projects/Veruthian.Library/Operations/OptionalOperation.cs
@@ -0,0 +1,16 @@
+namespace Veruthian.Library.Operations
+{
+    public class OptionalOperation<TState> : BaseNestedOperation<TState>
+    {
+        public OptionalOperation(IOperation<TState> operation) : base(operation) { }
+
+        public override string Description => $"optional({Operation})";
+
+        protected override bool DoAction(TState state, ITracer<TState> tracer = null)
+        {
+            Operation.Perform(state, tracer);
+
+            return true;
+        }
+    }
+}
