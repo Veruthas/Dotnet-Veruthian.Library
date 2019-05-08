813aefbcf9d1e9cf72210403676438d1886f79c3
Veruthas <veruthas@gmail.com>
2019-05-08 11:01:40 -0400

    Removed Operations.

--- Solution/Projects/_Console/Program.cs
+++ Solution/Projects/_Console/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Veruthian.Library.Readers.Extensions;
 
 namespace _Console
 {
@@ -7,8 +6,6 @@ namespace _Console
     {
         static void Main(string[] args)
         {
-            TestParsers.TestParser();
-
             Pause();
         }
 
