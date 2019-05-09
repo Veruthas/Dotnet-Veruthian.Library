8186ffad74653061ebb55f6ff61077241c317576
Veruthas <veruthas@gmail.com>
2019-05-09 16:44:26 -0400

    Added BaseMatchStepHandler

---
+++ Solution/Projects/Veruthian.Library/Steps/Matching/BaseMatchStepHandler.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Veruthian.Library.Steps.Matching
+{
+    public abstract class BaseMatchStepHandler<T> : IStepHandler
+    {
+        public bool? Handle(IStepWalker walker, IStep step)
+        {
+            switch (step)
+            {
+                case MatchStep<T> matcher:
+                    return HandleMatchStep(walker, matcher);
+                default:
+                    return null;
+            }
+        }
+
+        protected virtual bool HandleMatchStep(IStepWalker walker, MatchStep<T> matcher)
+        {
+            var item = PeekItem(walker);
+
+            var result = matcher.Match(item);
+
+            while (result.Success && result.State != null && MoveNextItem(walker))
+            {
+                item = PeekItem(walker);
+
+                result = matcher.Match(item);
+            }
+
+            return result.Success;
+        }
+
+        protected abstract T PeekItem(IStepWalker walker);
+
+        protected abstract bool MoveNextItem(IStepWalker walker);
+    }
+}
