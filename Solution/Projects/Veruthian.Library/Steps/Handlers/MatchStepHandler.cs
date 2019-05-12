eeaf25dc2206492a6da2d6af944749e28b45d8f5
Veruthas <veruthas@gmail.com>
2019-05-12 16:47:23 -0400

    Added Matched event to MatchStepHandler

--- Solution/Projects/Veruthian.Library/Steps/Handlers/MatchStepHandler.cs
+++ Solution/Projects/Veruthian.Library/Steps/Handlers/MatchStepHandler.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Veruthian.Library.Steps.Handlers
 {
     public abstract class MatchStepHandler<TState, T> : StepHandler<TState, MatchStep<T>>
@@ -10,6 +12,8 @@ namespace Veruthian.Library.Steps.Handlers
 
             while (result.Success && result.State != null && TryAdvance(state))
             {
+                OnMatched(item);
+
                 item = GetCurrent(state);
 
                 result = step.Match(item);
@@ -21,6 +25,13 @@ namespace Veruthian.Library.Steps.Handlers
         protected abstract T GetCurrent(TState state);
 
         protected abstract bool TryAdvance(TState state);
-    }
 
+        protected virtual void OnMatched(T item)
+        {
+            if (Matched != null)
+                Matched(item);
+        }
+
+        public event Action<T> Matched;
+    }
 }
