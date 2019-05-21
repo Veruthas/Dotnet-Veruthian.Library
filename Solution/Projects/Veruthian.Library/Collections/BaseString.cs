3f8ea616b2d7f343c53c5224fa5a0de1d2ca94b9
Veruthas <veruthas@gmail.com>
2019-05-21 16:01:46 -0400

    Added comparison to String

--- Solution/Projects/Veruthian.Library/Collections/BaseString.cs
+++ Solution/Projects/Veruthian.Library/Collections/BaseString.cs
@@ -5,13 +5,17 @@ using Veruthian.Library.Utility;
 
 namespace Veruthian.Library.Collections
 {
-    public abstract class BaseString<A, T, TVector> : BaseVector<A, T, TVector>, IEquatable<TVector>
+    public abstract class BaseString<A, T, TString> : BaseVector<A, T, TString>, IEquatable<TString>, IComparable<TString>
         where A : ISequential<A>
-        where TVector : BaseString<A, T, TVector>, new()
+        where TString : BaseString<A, T, TString>, new()
     {
-        private static readonly Func<T, T, bool> comparer = GetComparer();
+        public static readonly TString Empty = new TString();
 
-        private static Func<T, T, bool> GetComparer()
+        #region Equality
+
+        protected static EqualityComparison<T> EqualityComparer = GetDefaultEqualityComparison();
+
+        private static EqualityComparison<T> GetDefaultEqualityComparison()
         {
             if (typeof(IEquatable<T>).IsAssignableFrom(typeof(T)))
                 return (left, right) => ((IEquatable<T>)left).Equals(right);
@@ -19,24 +23,26 @@ namespace Veruthian.Library.Collections
                 return (left, right) => Equals(left, right);
         }
 
-        protected virtual bool ItemEquals(T left, T right) => comparer(left, right);
-
         private static IEnumerable<T> GetEnumerable(T[] array) => array;
 
-        private bool CompareEnumerators(IEnumerator<T> left, IEnumerator<T> right)
+        private static bool EnumeratorsEqual(EqualityComparison<T> equals, IEnumerator<T> left, IEnumerator<T> right)
         {
+            ExceptionHelper.VerifyNotNull(equals, nameof(equals));
+
             while (left.MoveNext())
             {
-                right.MoveNext();
+                if (!right.MoveNext())
+                    return false;
 
-                if (!ItemEquals(left.Current, right.Current))
+                if (!equals(left.Current, right.Current))
                     return false;
             }
 
             return !right.MoveNext();
         }
 
-        public bool Equals(TVector other)
+
+        public bool Equals(TString other, EqualityComparison<T> equals)
         {
             if (other == null)
                 return false;
@@ -52,11 +58,11 @@ namespace Veruthian.Library.Collections
 
                 var others = other.GetEnumerator();
 
-                return CompareEnumerators(items, others);
+                return EnumeratorsEqual(equals, items, others);
             }
         }
 
-        public bool Equals(IVector<A, T> other)
+        public bool EqualsVector(IVector<A, T> other, EqualityComparison<T> equals)
         {
             if (other == null)
                 return false;
@@ -68,12 +74,12 @@ namespace Veruthian.Library.Collections
 
                 var others = other.GetEnumerator();
 
-                return CompareEnumerators(items, others);
+                return EnumeratorsEqual(equals, items, others);
             }
         }
 
-        public bool Equals<AO>(IVector<AO, T> other)
-            where AO : ISequential<AO>
+        public bool EqualsVector<B>(IVector<B, T> other, EqualityComparison<T> equals)
+            where B : ISequential<B>
         {
             if (other == null)
                 return false;
@@ -85,11 +91,11 @@ namespace Veruthian.Library.Collections
 
                 var others = other.GetEnumerator();
 
-                return CompareEnumerators(items, others);
+                return EnumeratorsEqual(EqualityComparer, items, others);
             }
         }
 
-        public bool Equals(IVector<T> other)
+        public bool EqualsVector(IVector<T> other, EqualityComparison<T> equals)
         {
             if (other == null)
                 return false;
@@ -101,12 +107,11 @@ namespace Veruthian.Library.Collections
 
                 var others = other.GetEnumerator();
 
-                return CompareEnumerators(items, others);
+                return EnumeratorsEqual(EqualityComparer, items, others);
             }
         }
 
-
-        public bool Equals(T[] other)
+        public bool EqualsArray(T[] other, EqualityComparison<T> equals)
         {
             if (other == null)
                 return false;
@@ -118,36 +123,105 @@ namespace Veruthian.Library.Collections
 
                 var others = GetEnumerable(other).GetEnumerator();
 
-                return CompareEnumerators(items, others);
+                return EnumeratorsEqual(EqualityComparer, items, others);
             }
         }
 
-
-        public bool Equals(IEnumerable<T> other)
+        public bool EqualsEnumerable(IEnumerable<T> other, EqualityComparison<T> equals)
         {
             var items = this.GetEnumerator();
 
             var others = other.GetEnumerator();
 
-            return CompareEnumerators(items, others);
+            return EnumeratorsEqual(EqualityComparer, items, others);
         }
 
+
+
+        public bool Equals(TString other) => Equals(other, EqualityComparer);
+
+        public bool EqualsVector(IVector<A, T> other) => EqualsVector(other, EqualityComparer);
+
+        public bool EqualsVector<OA>(IVector<OA, T> other) where OA : ISequential<OA> => EqualsVector(other, EqualityComparer);
+
+        public bool EqualsVector(IVector<T> other) => EqualsVector(other, EqualityComparer);
+
+        public bool EqualsArray(T[] other) => EqualsArray(other, EqualityComparer);
+
+        public bool EqualsEnumerable(IEnumerable<T> other) => EqualsEnumerable(other, EqualityComparer);
 
         public override bool Equals(object other)
         {
             if (other == null)
                 return false;
-            else if (other is TVector)
-                return Equals((TVector)other);
+            else if (other is TString)
+                return Equals((TString)other);
             else if (other is IVector<T>)
-                return Equals((IVector<T>)other);
+                return EqualsVector((IVector<T>)other);
             else if (other is T[])
-                return Equals((T[])other);
+                return EqualsArray((T[])other);
             else if (other is IEnumerable<T>)
-                return Equals((IEnumerable<T>)other);
+                return EqualsEnumerable((IEnumerable<T>)other);
             else
                 return false;
         }
+
+        #endregion
+
+        #region Comparison
+
+        protected static Comparison<T> Comparer = GetDefaultComparison();
+
+        private static Comparison<T> GetDefaultComparison()
+        {
+            if (typeof(IComparable<T>).IsAssignableFrom(typeof(T)))
+                return (left, right) => ((IComparable<T>)left).CompareTo(right);
+            else
+                return (left, right) => 0;
+        }
+
+        public static int CompareEnumerable(Comparison<T> comparer, IEnumerator<T> left, IEnumerator<T> right)
+        {
+            ExceptionHelper.VerifyNotNull(comparer, nameof(comparer));
+
+            while (left.MoveNext())
+            {
+                if (!right.MoveNext())
+                    return 1;
+
+                var result = comparer(left.Current, right.Current);
+
+                if (result != 0)
+                    return result;
+            }
+
+            return right.MoveNext() ? -1 : 0;
+        }
+
+        public int CompareTo(TString other, Comparison<T> comparer)
+            => CompareEnumerable(comparer, this.GetEnumerator(), other.GetEnumerator());
+
+        public int CompareToVector(IVector<A, T> other, Comparison<T> comparer)
+            => CompareEnumerable(comparer, this.GetEnumerator(), other.GetEnumerator());
+
+        public int CompareToVector(IVector<T> other, Comparison<T> comparer)
+            => CompareEnumerable(comparer, this.GetEnumerator(), other.GetEnumerator());
+
+        public int CompareToVector<B>(IVector<B, T> other, Comparison<T> comparer) where B : ISequential<B>
+            => CompareEnumerable(comparer, this.GetEnumerator(), other.GetEnumerator());
+
+        public int CompareToArray(T[] other, Comparison<T> comparer) 
+            => CompareEnumerable(comparer, this.GetEnumerator(), GetEnumerable(other).GetEnumerator());
+
+        public int CompareToEnumeratble(IEnumerable<T> other, Comparison<T> comparer)
+            => CompareEnumerable(comparer, this.GetEnumerator(), other.GetEnumerator());
+
+        public int CompareTo(TString other)
+            => CompareTo(other, Comparer);
+
+        #endregion
+
+        #region Hashcode
 
         int hashcode;
 
@@ -158,5 +232,7 @@ namespace Veruthian.Library.Collections
         {
             this.hashcode = HashCodes.Default.Combine((int)this.Size, this.Items);
         }
+
+        #endregion
     }
 }
